78014e711bec363b970a811bffecac584f2476f1
yourusername <jorgesilva161@hotmail.com>
2025-07-27 23:02:02 -0600

    GPS Tracker 1.6

--- static/map.js
+++ static/map.js
@@ -1,5 +1,6 @@
 const map = L.map('map').setView([14.0723, -87.1921], 13);
 
+// Crear el ícono personalizado
 const customIcon = L.icon({
     iconUrl: '/static/icono_gps.png',
     iconSize: [32, 32],
@@ -7,17 +8,16 @@ const customIcon = L.icon({
     popupAnchor: [0, -32]
 });
 
-L.marker([device.lat, device.lon], { icon: customIcon }).addTo(map);
-
-
 async function loadPositions() {
   const response = await fetch('/api/positions');
   const data = await response.json();
 
   data.forEach(d => {
-    L.marker([d.lat, d.lon]).addTo(map)
+    // Aplicar el ícono personalizado aquí
+    L.marker([d.lat, d.lon], { icon: customIcon }).addTo(map)
       .bindPopup(`<b>${d.device_id}</b><br>${d.event}<br>${d.timestamp}`);
   });
 }
 
 loadPositions();
+
