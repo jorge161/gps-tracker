2b6c4168a86dfb50a325d048826e2ca64e1a3d97
yourusername <jorgesilva161@hotmail.com>
2025-07-27 23:10:06 -0600

    GPS Tracker 1.7

--- static/map.js
+++ static/map.js
@@ -1,11 +1,14 @@
 const map = L.map('map').setView([14.0723, -87.1921], 13);
 
-// Crear el ícono personalizado
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+  attribution: '© OpenStreetMap contributors'
+}).addTo(map);
+
 const customIcon = L.icon({
-    iconUrl: '/static/icono_gps.png',
-    iconSize: [32, 32],
-    iconAnchor: [16, 32],
-    popupAnchor: [0, -32]
+  iconUrl: '/static/icono_gps.png',
+  iconSize: [32, 32],
+  iconAnchor: [16, 32],
+  popupAnchor: [0, -32]
 });
 
 async function loadPositions() {
@@ -13,11 +16,11 @@ async function loadPositions() {
   const data = await response.json();
 
   data.forEach(d => {
-    // Aplicar el ícono personalizado aquí
     L.marker([d.lat, d.lon], { icon: customIcon }).addTo(map)
       .bindPopup(`<b>${d.device_id}</b><br>${d.event}<br>${d.timestamp}`);
   });
 }
 
 loadPositions();
+
 
