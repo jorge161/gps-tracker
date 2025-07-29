2f08b7088ccda66235ec8f1851c5137b6df87d5f
yourusername <jorgesilva161@hotmail.com>
2025-07-28 19:21:36 -0600

    GPS Tracker 2.1

--- static/map.js
+++ static/map.js
@@ -11,15 +11,33 @@ const customIcon = L.icon({
   popupAnchor: [0, -32]
 });
 
+
+
 async function loadPositions() {
   const response = await fetch('/api/positions');
   const data = await response.json();
 
   data.forEach(d => {
-    L.marker([d.lat, d.lon], { icon: customIcon }).addTo(map)
-      .bindPopup(`<b>${d.device_id}</b><br>${d.event}<br>${d.timestamp}`);
+    // Verificamos si el evento contiene "Intrusión"
+    if (d.event && d.event.toLowerCase().includes("intrusión")) {
+      // Crear un ícono HTML personalizado con animación
+      const alertIcon = L.divIcon({
+        className: '',  // No clases adicionales
+        html: '<div class="alert-circle"></div>',
+        iconSize: [20, 20],
+        iconAnchor: [10, 10]
+      });
+
+      L.marker([d.lat, d.lon], { icon: alertIcon }).addTo(map)
+        .bindPopup(`<b>${d.device_id}</b><br><strong style="color:red">${d.event}</strong><br>${d.timestamp}`);
+    } else {
+      // Ícono normal
+      L.marker([d.lat, d.lon], { icon: customIcon }).addTo(map)
+        .bindPopup(`<b>${d.device_id}</b><br>${d.event}<br>${d.timestamp}`);
+    }
   });
 }
+
 
 loadPositions();
 
